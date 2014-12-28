15150d53a760f3077441661a054598f4f1918d7e
liurong <570899307@qq.com>
2014-12-28 13:39:11 -0500

    test JudgeNumber()_getScore()_1A1B

--- __tests__/judge-number-tests.js
+++ __tests__/judge-number-tests.js
@@ -21,5 +21,15 @@ describe('JudgeNumber', function() {
 
       expect(result).toEqual('0A0B');
     });
+
+    it('should return the 1A1B', function() {
+
+      var JudgeNumber = require('../src/judge-number');
+      var judgeNumber = new JudgeNumber();
+
+      var result = judgeNumber.getScore([1,3,5,6],[1,2,3,4]);
+
+      expect(result).toEqual('1A1B');
+    });
   });
 });
--- src/judge-number.js
+++ src/judge-number.js
@@ -3,21 +3,23 @@ var _ = require('lodash');
 function JudgeNumber() {
 }
 JudgeNumber.prototype.getScore = function (inputArrays, randomNumberArrays) {
-  if (inputArrays.toString() === randomNumberArrays.toString()) {
-    return '4A0B';
+  var aCount = 0;
+  for(var i = 0; i < inputArrays.length; i++) {
+    if (randomNumberArrays[i] === inputArrays[i]) {
+      aCount++;
+    }
   }
 
   var count = 0;
-  _.forEach(randomNumberArrays, function(randomNumberArray) {
-    var isExit = _.contains(_.map(inputArrays),randomNumberArray);
-    if (isExit) {
-      count++;
+  for(var j = 0; j < randomNumberArrays.length; j++){
+    for(var x = 0; x < inputArrays.length; x++) {
+      if(inputArrays[x] === randomNumberArrays[j]){
+        count ++;
+      }
     }
-  });
-  if (count === 0) {
-    return '0A0B';
   }
-  return null;
+
+  return aCount + 'A' + (count - aCount) + 'B';
 };
 
 module.exports = JudgeNumber;
