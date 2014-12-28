e08824407e506274b365473352643641df3ae3d9
liurong <570899307@qq.com>
2014-12-28 17:50:04 -0500

    test RandomNumbre()_getRandomNuberArray()_is_not_repeat

--- __tests__/random-number-tests.js
+++ __tests__/random-number-tests.js
@@ -2,13 +2,30 @@ jest.dontMock('../src/random-number');
 
 describe('RandomNumber', function() {
   describe('#getRandomNumberArray', function() {
-    it('should return the randomNumberIsTrue', function() {
+    it('should return the randomNumberIsLength', function() {
       var RandomNumber = require('../src/random-number.js');
       var randomNumber = new RandomNumber();
 
       var result = randomNumber.getRandomNumberArray();
 
       expect(result.length).toBe(4);
+    });
+
+    it('should return the randomNumberIsNotRepeat', function() {
+      var RandomNumber = require('../src/random-number.js');
+      var randomNumber = new RandomNumber();
+      var count = 0;
+      var result = randomNumber.getRandomNumberArray();
+
+      for(var i = 0; i < result.length; i++) {
+        for (var j = 0; j < result.length; j++) {
+          if (result[i] === result[j] && i !== j) {
+            count ++;
+          }
+        }
+      }
+
+      expect(count).toBe(0);
     });
   });
 });
