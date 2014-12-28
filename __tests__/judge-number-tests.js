3b9bc933f258f2745c72d66a78242b6e55aeef26
liurong <570899307@qq.com>
2014-12-28 02:05:20 -0500

    test JudgeNumber()_getScore()_0A0B

--- __tests__/judge-number-tests.js
+++ __tests__/judge-number-tests.js
@@ -2,7 +2,7 @@ jest.dontMock('../src/judge-number');
 
 describe('JudgeNumber', function() {
   describe('#getScore', function() {
-    it('should return the 0A4B', function() {
+    it('should return the 4A0B', function() {
 
       var JudgeNumber = require('../src/judge-number');
       var judgeNumber = new JudgeNumber();
@@ -10,6 +10,16 @@ describe('JudgeNumber', function() {
       var result = judgeNumber.getScore([1,2,3,4],[1,2,3,4]);
 
       expect(result).toEqual('4A0B');
+    });
+
+    it('should return the 0A0B', function() {
+
+      var JudgeNumber = require('../src/judge-number');
+      var judgeNumber = new JudgeNumber();
+
+      var result = judgeNumber.getScore([1,2,3,4],[5,6,7,8]);
+
+      expect(result).toEqual('0A0B');
     });
   });
 });
--- src/judge-number.js
+++ src/judge-number.js
@@ -1,11 +1,21 @@
-function JudgeNumber() {
-  // this.inputArrays = inputArrays;
-  // this.randomNumberArrays = randomNumberArrays;
-}
+var _ = require('lodash');
 
+function JudgeNumber() {
+}
 JudgeNumber.prototype.getScore = function (inputArrays, randomNumberArrays) {
   if (inputArrays.toString() === randomNumberArrays.toString()) {
     return '4A0B';
+  }
+
+  var count = 0;
+  _.forEach(randomNumberArrays, function(randomNumberArray) {
+    var isExit = _.contains(_.map(inputArrays),randomNumberArray);
+    if (isExit) {
+      count++;
+    }
+  });
+  if (count === 0) {
+    return '0A0B';
   }
   return null;
 };
