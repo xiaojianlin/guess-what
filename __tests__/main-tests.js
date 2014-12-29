1e7275d678671368d0206b230488b749f99cae97
liurong <570899307@qq.com>
2014-12-28 23:41:24 -0500

    test guessNumber()_sorry!_But_we_given_you_presentation_2A2B,_please_again

--- __tests__/main-tests.js
+++ __tests__/main-tests.js
@@ -20,4 +20,15 @@ describe('.guessNumber', function() {
     expect(result).toEqual('congratulation !!!');
 
   });
+
+  it('should return sorry!_But_we_given_you_presentation_2A2B,_please_again!', function() {
+    randomNumberArrays = [1,2,3,4];
+    inputArrays = [1,2,4,3];
+
+    var answer = judgeNumber.getScore(inputArrays,randomNumberArrays);
+    var result = printText.getText(answer);
+
+    expect(result).toEqual('sorry! But we given you presentation 2A2B, please again!');
+
+  });
 });
